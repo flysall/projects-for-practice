9aa4e81da07aa63f3184e9366c27d9bb10970fa1
flysall <flysall@163.com>
2017-08-01 15:49:53 +0800

    Upload all kinds of Piece and modify PieceCreatorImpl.java

--- tetris/src/flysall/tetris/object/impl/PieceCreatorImpl.java
+++ tetris/src/flysall/tetris/object/impl/PieceCreatorImpl.java
@@ -7,6 +7,81 @@ import flysall.tetris.object.*;
 import flysall.tetris.piece.*;
 import flysall.tetris.util.ImageUtil;
 
-public class PieceCreatorImpl {
-
+public class PieceCreatorImpl implements PieceCreator{
+	//缓存每个Square
+	private Map<Integer, Image> images = new HashMap<Integer, Image>();
+	
+	//共计七种颜色方案
+	private final static int COLOR_SIZE = 7;
+	
+	//共计七种大方块
+	private final static int SQUARE_SIZE = 7;
+	private Random random = new Random();
+	
+	@Override 
+	public Piece createPiece(int x, int y) {
+		Image image = getImage(random.nextInt(COLOR_SIZE));
+		Piece piece = initPiece(image);
+		piece.setSquaresXLocation(x);
+		piece.setSquaresYLocation(y);
+		return piece;
+	}
+	
+	private Piece initPiece(Image image) {
+		Piece piece = null;
+		int pieceType = random.nextInt(SQUARE_SIZE); 
+		//初始化 Piece，随机创建各个大方块
+		if (pieceType == 0){
+			piece = new Piece0(image);
+		} else if (pieceType == 1) {
+			piece = new Piece1(image);
+		} else if (pieceType == 2) {
+			piece = new Piece2(image);
+		} else if (pieceType == 3) {
+			piece = new Piece3(image);
+		} else if (pieceType == 4) {
+			piece = new Piece4(image);
+		} else if (pieceType == 5) {
+			piece = new Piece5(image);
+		} else if (pieceType == 6) {
+			piece = new Piece6(image);
+		}
+		return piece;
+	}
+	
+	/**
+	 * 从map中得到图片对象，如果map中没有存在图片对象, 则创建
+	 * @param key
+	 * @return
+	 */
+	private Image getImage(int key) {
+		if (this.images.get(key) == null) {
+			Image s = ImageUtil.getImage("images/square" + key + ".jpg");
+			this.images.put(key, s);
+		}
+		return this.images.get(key);
+	}
+	
+	@Override 
+	public Piece getPiece() {
+		return null;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tetris/src/flysall/tetris/piece/Piece3.java
@@ -0,0 +1,26 @@
+package flysall.tetris.piece;
+
+import java.awt.Image;
+import java.util.*;
+
+import flysall.tetris.object.*;
+
+
+public class Piece3 extends Piece{
+	public Piece3(Image image) {
+		//第一种变化
+		List<Square> squares0 = new ArrayList<Square>();
+		squares0.add(new Square(image, image.getWidth(null), 0));
+		squares0.add(new Square(image, image.getWidth(null)*2, 0));
+		squares0.add(new Square(image, 0, image.getHeight(null)));
+		squares0.add(new Square(image, image.getWidth(null), image.getHeight(null)));
+		List<Square> squares1 = new ArrayList<Square>();
+		squares1.add(new Square(image, 0, 0));
+		squares1.add(new Square(image, 0, image.getHeight(null)));
+		squares1.add(new Square(image, image.getWidth(null), image.getHeight(null)));
+		squares1.add(new Square(image, image.getWidth(null), image.getHeight(null)*2));
+		super.changes.add(squares0);
+		super.changes.add(squares1);
+		super.setSquares(getDefault());
+	}
+}
