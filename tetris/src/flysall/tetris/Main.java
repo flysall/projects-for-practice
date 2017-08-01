3ade335cdefc72a869481ebb520c8f606789b134
flysall <flysall@163.com>
2017-08-01 21:17:51 +0800

    tetris: finish all code with some bug

---
+++ tetris/src/flysall/tetris/Main.java
@@ -0,0 +1,10 @@
+package flysall.tetris;
+
+import flysall.tetris.ui.*;
+
+public class Main {
+	public static void main(String[] args) {
+		MainFrame m = new MainFrame();
+		m.setVisible(true);
+	}
+}
