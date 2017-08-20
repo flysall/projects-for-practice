d489dd93a155eb9317deeff0a7dd8df4fc9536b3
flysall <flysall@163.com>
2017-08-20 16:17:34 +0800

    mycrawl: debug

--- mycrawl/src/main/java/com/crawl/proxy/ProxyPool.java
+++ mycrawl/src/main/java/com/crawl/proxy/ProxyPool.java
@@ -29,23 +29,21 @@ public class ProxyPool {
 	 */
 	public final static DelayQueue<Proxy> proxyQueue = new DelayQueue();
 	public final static Map<String, Class> proxyMap = new HashMap<>();
-	static {
-		int pages = 8;
-		for (int i = 1; i <= pages; i++){
-			proxyMap.put("http://www.xicidaili.com/wt/" + i + ".html", XicidailiProxyListPageParser.class);
-			proxyMap.put("http://www.xicidaili.com/nn/" + i + ".html", XicidailiProxyListPageParser.class);
-			proxyMap.put("http://www.xididaili.com/wn/" + i + ".html", XicidailiProxyListPageParser.class);
-			proxyMap.put("http://www.xicidaili.com/nt/" + i + ".html",  XicidailiProxyListPageParser.class);
-			proxyMap.put("http://www.ip181.com/daili/" + i + ".html", Ip181ProxyListPageParser.class);
-			proxyMap.put("http://www.mimiip.com.com/gngao/" + i, MimiipProxyListPageParser.class);//高匿
-			proxyMap.put("http://www.mimiip.com/gnpu/" + i, MimiipProxyListPageParser.class);//普匿
-			proxyMap.put("http://www.66ip.cn/" + i + ".html", Ip66ProxyListPageParser.class);
-			for (int j = 1; j < 34; j++) {
-				proxyMap.put("http://www.66ip.cn/areindex_" + j + "/" + i + ".html", Ip66ProxyListPageParser.class);
-			}
-		}
-		proxyQueue.add(new Direct(TIME_INTERVAL));
-	}
+    static {
+        int pages = 8;
+        for(int i = 1; i <= pages; i++){
+            proxyMap.put("http://www.xicidaili.com/wt/" + i + ".html", XicidailiProxyListPageParser.class);
+            proxyMap.put("http://www.xicidaili.com/nn/" + i + ".html", XicidailiProxyListPageParser.class);
+            proxyMap.put("http://www.xicidaili.com/wn/" + i + ".html", XicidailiProxyListPageParser.class);
+            proxyMap.put("http://www.xicidaili.com/nt/" + i + ".html", XicidailiProxyListPageParser.class);
+            proxyMap.put("http://www.ip181.com/daili/" + i + ".html", Ip181ProxyListPageParser.class);
+            proxyMap.put("http://www.mimiip.com/gngao/" + i, MimiipProxyListPageParser.class);//高匿
+            proxyMap.put("http://www.mimiip.com/gnpu/" + i, MimiipProxyListPageParser.class);//普匿
+            proxyMap.put("http://www.66ip.cn/" + i + ".html", Ip66ProxyListPageParser.class);
+            for(int j = 1; j < 34; j++){
+                proxyMap.put("http://www.66ip.cn/areaindex_" + j + "/" + i + ".html", Ip66ProxyListPageParser.class);
+            }
+        }
 }
 
 
