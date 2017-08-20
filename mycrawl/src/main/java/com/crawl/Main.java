b352e42d8893ae0fa1bbc774440cba58da858175
flysall <flysall@163.com>
2017-08-20 09:55:49 +0800

    mycrawl: debug in Main

---
+++ mycrawl/src/main/java/com/crawl/Main.java
@@ -0,0 +1,18 @@
+package com.crawl;
+
+import com.crawl.core.util.Config;
+import com.crawl.core.util.SimpleLogger;
+import com.crawl.proxy.ProxyHttpClient;
+import com.crawl.zhihu.ZhiHuHttpClient;
+import org.apache.log4j.Logger;
+
+/**
+ * 爬虫入口
+ */
+public class Main {
+	private static Logger logger = SimpleLogger.getSimpleLogger(Main.class);
+	public static void main(String[] args){
+		ProxyHttpClient.getInstance().startCrawl();
+		ZhiHuHttpClient.getInstance().startCrawl();
+	}
+}
